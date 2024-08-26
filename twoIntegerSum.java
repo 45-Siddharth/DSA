faec74a0c81272643e519fa8eead7919419dd163
Siddharth <kinthadasiddardha7@gmail.com>
2024-08-26 13:32:13 +0530

    3rd problem

---
+++ twoIntegerSum.java
@@ -0,0 +1,18 @@
+package DSA;
+
+import java.util.HashMap;
+
+public class twoIntegerSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hm.containsKey(target-nums[i]))
+            {
+                return new int[] {hm.get(target-nums[i]),i};
+            }
+            hm.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+}
